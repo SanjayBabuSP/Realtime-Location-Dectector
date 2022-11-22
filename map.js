3a0f1299bf747a1782813ad72561e1f5544519e9
SanjayBabuSP <SanjayBabuSP@gmail.com>
2022-11-22 22:50:41 +0530

    Forth commit - API changes

--- map.js
+++ map.js
@@ -8,10 +8,16 @@
  });
  osm.addTo(map); */
 
+ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map); 
+
+ /*
  var osm = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
  maxZoom: 20,
  attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo(map); 
+})
+*/
 
  if(!navigator.geolocation) {
      console.log("Your browser doesn't support geolocation feature!")
